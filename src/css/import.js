00fa7f5d12cd62318be86137e19a71488b986eb2
Ao02 <uroboros0622@gmail.com>
2020-03-21 18:24:07 +0900

    import作成

--- src/css/import.js
+++ src/css/import.js
@@ -1,5 +1,5 @@
 // ▼ CSSの読み込み方法
 // import 'CSSファイル名'
-import 'css/normalize.css'
-import 'css/skelton.css'
-import 'css/sample.css'
+import './base/normalize.css';
+import './base/skeleton.css';
+import './top.css';
